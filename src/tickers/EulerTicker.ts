9a064e0cb91168c638339fa061400b183f3ac7fd
David Polák <git@polakdavid.com>
2021-10-30 21:19:41 +0200

    fix: dev5 x

--- src/tickers/EulerTicker.ts
+++ src/tickers/EulerTicker.ts
@@ -2,28 +2,40 @@ import { AbstractNode } from "../nodes/AbstractNode"
 import { AbstractTicker } from "./AbstractTicker"
 
 export class EulerTicker extends AbstractTicker {
+  /* This class originally relied on Vector2 operations, however profiling
+  showed that any function calls or object creation in here is expensive. */
+
   tickSingle(tDelta: number, node: AbstractNode) {
-    node.position.add(node.velocity.clone().multiplyByScalar(tDelta / 1000))
+    const scalar = tDelta / 1000
+    node.position.x = node.position.x + node.velocity.x * scalar
+    node.position.y = node.position.y + node.velocity.y * scalar
   }
 
   tickBoth(tDelta: number, nodeA: AbstractNode, nodeB: AbstractNode) {
-    const distance = nodeA.position.distance(nodeB.position)
+    const distanceX = nodeA.position.x - nodeB.position.x
+    const distanceY = nodeA.position.y - nodeB.position.y
+    const distance = Math.sqrt(distanceX * distanceX + distanceY * distanceY)
 
     const attraction = 10
 
-    if (distance > 10) {
+    if (distance > 10 && distance < 100) {
       const scalar = Math.pow(distance, -3) * attraction * tDelta
 
-      const difference = nodeA.position.clone().subtract(nodeB.position)
-      const acceleration = difference.multiplyByScalar(scalar)
+      const accelerationX = (nodeA.position.x - nodeB.position.x) * scalar
+      const accelerationY = (nodeA.position.y - nodeB.position.y) * scalar
 
-      const forceA = acceleration.clone().multiplyByScalar(nodeA.mass).negate()
-      const forceB = acceleration.multiplyByScalar(nodeB.mass)
+      const forceAx = -(accelerationX * nodeA.mass)
+      const forceAy = -(accelerationY * nodeA.mass)
 
-      nodeA.velocity.add(forceA)
-      nodeB.velocity.add(forceB)
+      const forceBx = accelerationX * nodeB.mass
+      const forceBy = accelerationY * nodeB.mass
 
-      return forceA.magnitude() * 1.5
+      nodeA.velocity.x += forceAx
+      nodeA.velocity.y += forceAy
+      nodeB.velocity.x += forceBx
+      nodeB.velocity.y += forceBy
+
+      return Math.sqrt(forceAx * forceAx + forceAy * forceAy) * 1.5
     }
 
     return 0
