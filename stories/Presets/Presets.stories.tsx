6d987ee914de63e4dddd6e6acbcf3ce1a3dc446a
David Polák <git@polakdavid.com>
2021-11-01 17:31:58 +0100

    docs: add the GravityChill preset

---
+++ stories/Presets/Presets.stories.tsx
@@ -0,0 +1,58 @@
+// @ts-ignore
+import React from "react"
+
+import { NodalBackgroundWrapper } from "../NodalBackgroundWrapper"
+import { ArgumentTypes, DefaultArguments } from "../ArgumentTypes"
+import { NodalBackgroundMode } from "../../src/main/NodalBackground"
+import ReactMarkdown from "react-markdown"
+
+import "./Presets.css"
+
+export default {
+  title: "Presets",
+  argTypes: {
+    ...ArgumentTypes,
+  },
+  args: {
+    ...DefaultArguments,
+  },
+  parameters: {
+    controls: { expanded: true },
+    previewTabs: { "storybook/docs/panel": { hidden: true } },
+    backgrounds: { disable: true },
+  },
+}
+
+export const GravityChill = (props: any) => {
+  const overrides = {
+    mode: NodalBackgroundMode.Gravity,
+    numberOfNodes: 130,
+    nodeVisualSize: 1.5,
+    simMaxDistance: 85,
+    simMinDistance: 10,
+    simAttraction: 3,
+  }
+  props = { ...props, ...overrides }
+
+  const description = `
+  ## Gravity Chill
+  Gravity mode with less movement and larger node sizes.
+  \`\`\`
+  mode: NodalBackgroundMode.Gravity,
+  numberOfNodes:  130,
+  nodeVisualSize:  1.5,
+  simMaxDistance:  85,
+  simMinDistance:  10,
+  simAttraction:  3
+  \`\`\`
+  `
+
+  return (
+    <>
+      <NodalBackgroundWrapper {...props} />
+      <div id="presets-content">
+        <ReactMarkdown>{description}</ReactMarkdown>
+      </div>
+    </>
+  )
+}
