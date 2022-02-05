73f6f3593f4da65acbfb6c29c718fb0752b0a66f
David Polák <git@polakdavid.com>
2022-02-05 03:21:48 +0100

    docs: update storybook theme and favicon

---
+++ .storybook/theme.js
@@ -0,0 +1,8 @@
+import { create } from '@storybook/theming';
+
+export default create({
+  base: 'light',
+  brandTitle: 'Nodal Background',
+  brandUrl: 'https://nodal-background.polakdavid.com',
+  brandImage: null,
+});
